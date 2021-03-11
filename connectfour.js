2fd5836251dda3d8abb4933bfa5c54ac7689cf65
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2021-03-11 12:49:20 +0100

    win check algorithm

--- connectfour.js
+++ connectfour.js
@@ -1,38 +1,38 @@
 const oldBoard = [
-    [0, 0, 0, 0, "a", "b"],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
+    ["a", "a", 0, "a", "a", 0],
+    [0, "a", 0, 0, 0, 0],
+    [0, 0, "a", 0, 0, 0],
+    [0, 0, "a", "b", "b", 0],
+    [0, "a", 0, "a", "b", 0],
+    [0, "a", "a", 0, "a", 0],
+    [0, "a", 0, 0, 0, "a"],
 ];
 
 const newBoard = [
     [0, 0, 0, 0, "a", "b"],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, "a"],
+    [0, 0, 0, 0, "a", 0],
+    ["a", 0, "b", "a", "a", "a"],
+    [0, 0, "a", "a", 0, 0],
+    [0, 0, "b", 0, 0, 0],
+    [0, "a", 0, 0, 0, 0],
+    ["a", 0, 0, 0, 0, "a"],
 ];
 
 const player = "a";
 
-const result = checkMove(oldBoard, newBoard, player);
-console.log(result);
+// const result = checkMove(oldBoard, newBoard, player);
+// console.log(result);
 
 function checkMove(oldBoard, newBoard, player) {
     let countDiff = 0;
     for (let row = 0; row < 7; row++) {
-        for (let fields = 0; fields < 6; fields++) {
-            if (oldBoard[row][fields] !== newBoard[row][fields]) {
-                // check all fields below an break
-                // if fields below are not filled, return false
-                // if all fields are filled countDiff ++
-                if(newBoard[row][fields] !== player) return false;
-                for (let i = fields; i < 6; i++) {
+        for (let field = 0; field < 6; field++) {
+            if (oldBoard[row][field] !== newBoard[row][field]) {
+                // check all field below an break
+                // if field below are not filled, return false
+                // if all field are filled countDiff ++
+                if (newBoard[row][field] !== player) return false;
+                for (let i = field; i < 6; i++) {
                     if (newBoard[row][i] !== "a" && newBoard[row][i] !== "b") return false;
                 }
                 countDiff++;
@@ -41,4 +41,75 @@ function checkMove(oldBoard, newBoard, player) {
         }
     }
     return countDiff === 1;
+}
+
+const result = hasWon(newBoard, player);
+console.log(result);
+
+function hasWon(board, player) {
+
+    let winCounter;
+
+    // vertikal
+    for (let row = 0; row < 7; row++) {
+        winCounter = 0;
+        for (let line = 0; line < 6; line++) {
+            if (board[row][line] === player) {
+                winCounter++;
+                if (winCounter >= 4) return true;
+            } else {
+                winCounter = 0;
+            }
+        }
+    }
+
+    // horizontal
+    for (let line = 0; line < 6; line++) {
+        winCounter = 0;
+        for (let row = 0; row < 7; row++) {
+            if (board[row][line] === player) {
+                winCounter++;
+                if (winCounter >= 4) return true;
+            } else {
+                winCounter = 0;
+            }
+        }
+    }
+
+    // diagonal rechts
+    for (let row = 0; row < 4; row++) {
+        for (let line = 3; line < 6; line++) {
+            if (board[row][line] === player) {
+                if ((board[row][line] === player)
+                    && (board[row + 1][line - 1] === player)
+                    && (board[row + 2][line - 2] === player)
+                    && (board[row + 3][line - 3] === player)) return true;
+            }
+        }
+    }
+
+    // diagonal links
+    for (let row = 0; row < 4; row++) {
+        for (let line = 0; line < 3; line++) {
+            if (board[row][line] === player) {
+                if ((board[row][line] === player)
+                    && (board[row + 1][line + 1] === player)
+                    && (board[row + 2][line + 2] === player)
+                    && (board[row + 3][line + 3] === player)) return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+function checkDraw(board) {
+    for (let row = 0; row < 7; row++) {
+        for (let line = 0; line < 6; line++) {
+            if (board[row][line] === 0) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
